d75b4a3868293bef4326348e39c8d0c841c1672e
Colin Smith <colinsmith@MacBook-Pro-4.local>
2019-03-08 15:44:37 -0700

    Adds my first Git commit of my Career

---
+++ GitWorkshop/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitWorkshop
+//
+//  Created by Colin Smith on 3/8/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func partyOn(){
+        print("We are partying on the Git")
+    }
+
+}
+
